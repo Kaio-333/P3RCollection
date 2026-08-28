7d154a1b38d1237276e29b09999e5c8784c8151e
kaio <dopperkaio@gmail.com>
2026-08-28 19:03:59 -0300

    ci: configure GitHub Pages deployment and update vite base path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "/P3RCollection/",
   publicDir: "assets",
   plugins: [react(), tailwindcss()],
 });
